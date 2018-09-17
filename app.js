1f9081125ed6c0b838274c1445950854d60d921f
Sunil Jadhav <sunil.jadhav@infostretch.com>
2018-09-17 19:06:01 +0530

    updated files

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var express = require('express');
 app = express(),
 port = process.env.PORT || 3000,
 path = require('path');
-loginController = require('./controller/loginController');
+loginController = require('./controller');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname, "js")));
-loginController(app);
+loginController.logincontroller(app);
 app.listen(port);
 console.log('Server started on: ' + port);
